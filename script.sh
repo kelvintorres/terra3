a292a07e858c02b6033f0913e122bf43bb2e76cc
kelvintorres <90866558+kelvintorres@users.noreply.github.com>
2023-03-18 23:16:34 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Instalar Terraform
-wget https://releases.hashicorp.com/terraform/1.4.2/terraform_1.4.2_linux_amd64.zip
-unzip terraform_1.0.10_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
+unzip terraform_0.11.11_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 
 # Ejecutar Terraform
